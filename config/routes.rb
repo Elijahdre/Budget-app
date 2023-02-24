0b1e737df6323e958f47f23d8f94a26b8c901b38
Elijah Odjegba <elijahodjegba@gmail.com>
2023-02-24 23:24:52 +0100

    Build logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, sign_out_via: [:get]
+
+  root to: 'home#index'
+
+  resources :categories, only: %i[new create index] do
+    resources :transactions, only: %i[index create new]
+  end
+
+  resources :users, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
