63752728ba24caa5aa619150a97b26f05e0e4d7c
Elijah Odjegba <elijahodjegba@gmail.com>
2023-02-25 07:05:49 +0100

    Fix stylelint error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, except: :index
-  
-    def index
-      redirect_to new_user_registration_path
-    end
+  before_action :authenticate_user!, except: :index
+
+  def index
+    redirect_to new_user_registration_path
   end
-  
+end
